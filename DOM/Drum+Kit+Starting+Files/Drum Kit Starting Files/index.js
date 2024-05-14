8af9e1cd1aa6013c0fd9d7c8789160075a1d0ce8
SuhasPalani <sugipals007@gmail.com>
2024-05-14 00:26:53 -0500

    working with buttons

--- DOM/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ DOM/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -1,7 +1,39 @@
+var drums = document.querySelectorAll(".drum");
 
-for(var i=0; i < document.querySelectorAll(".drum").length; i++) {  
-document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-  alert("I got clicked!");
-});
+function playSound(soundFile) {
+    var audio = new Audio(soundFile);
+    audio.play().catch(function(error) {
+        console.error('Error playing sound:', error);
+    });
+}
 
+for (var i = 0; i < drums.length; i++) {
+  drums[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+    switch (buttonInnerHTML) {
+      case "w":
+        playSound("sounds/tom-1.mp3");
+        break;
+      case "a":
+        playSound("sounds/tom-2.mp3");
+        break;
+      case "s":
+        playSound("sounds/tom-3.mp3");
+        break;
+      case "d":
+        playSound("sounds/tom-4.mp3");
+        break;
+      case "j":
+        playSound("sounds/snare.mp3");
+        break;
+      case "k":
+        playSound("sounds/kick-bass.mp3");
+        break;
+      case "l":
+        playSound("sounds/crash.mp3");
+        break;
+      default:
+        console.log(buttonInnerHTML);
+    }
+  });
 }
