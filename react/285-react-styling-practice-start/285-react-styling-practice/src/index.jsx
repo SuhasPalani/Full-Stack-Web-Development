db70536f2f7e917ac60468c4219e0584d56bfe30
SuhasPalani <sugipals007@gmail.com>
2024-05-25 15:32:38 -0500

    solution

--- react/285-react-styling-practice-start/285-react-styling-practice/src/index.jsx
+++ react/285-react-styling-practice-start/285-react-styling-practice/src/index.jsx
@@ -1,13 +1,29 @@
-//Create a React app from scratch.
-//Show a single h1 that says "Good morning" if between midnight and 12PM.
-//or "Good Afternoon" if between 12PM and 6PM.
-//or "Good evening" if between 6PM and midnight.
-//Apply the "heading" style in the styles.css
-//Dynamically change the color of the h1 using inline css styles.
-//Morning = red, Afternoon = green, Night = blue.
+import React from "react";
+import ReactDOM from "react-dom";
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+
+const today = new Date();
+const hour = today.getHours();
+
+let greeting;
+const styles = {};
+
+if (hour >= 0 && hour < 12) {
+  greeting = "Good morning";
+  styles.color = "red";
+} else if (hour >= 12 && hour < 18) {
+  greeting = "Good Afternoon";
+  styles.color = "green";
+} else {
+  greeting = "Good evening";
+  styles.color = "blue";
+}
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading" style={styles}>
+      {greeting}
+    </h1>
+  </div>,
+  document.getElementById("root")
+);
