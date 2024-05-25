6205d547b8190d05808a49abd993160755f4bfa5
SuhasPalani <sugipals007@gmail.com>
2024-05-25 13:49:29 -0500

    solution

--- react/282-javascript-expressions-in-jsx-practice-start/282-javascript-expressions-in-jsx-practice/src/index.jsx
+++ react/282-javascript-expressions-in-jsx-practice-start/282-javascript-expressions-in-jsx-practice/src/index.jsx
@@ -12,3 +12,19 @@
 // to install the node modules and
 // npm run dev
 // to launch your react project in your browser
+import React from "react";
+import ReactDom from "react-dom";
+const today=new Date();
+const fname='Suhas';
+const lname='Palani';
+const year=today.getFullYear();
+
+
+ReactDom.render(
+    <div>
+        <p>Created by {fname} {lname}</p>
+        <p>Copyright {year}</p>
+    </div>,
+    document.getElementById('root')
+
+);
