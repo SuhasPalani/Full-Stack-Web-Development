5355377b73249a4918bd1c0c83d720d706789cec
SuhasPalani <sugipals007@gmail.com>
2024-05-25 16:24:58 -0500

    custom components

---
+++ react/286-react-components-start/286-react-components/src/App.jsx
@@ -0,0 +1,15 @@
+import Heading from "./Heading";
+import List from "./List";
+import React from "react";
+
+
+function App(){
+    return (
+        <div>
+            <Heading />
+            <List />
+        </div>
+    )
+};
+
+export default App;
