4c4ca728301c6aac08a7a61a026d1fa06a27ab7c
SuhasPalani <sugipals007@gmail.com>
2024-05-25 12:30:54 -0500

    solution

--- react/281-javascript-expressions-in-jsx-start/281-javascript-expressions-in-jsx/src/index.jsx
+++ react/281-javascript-expressions-in-jsx-start/281-javascript-expressions-in-jsx/src/index.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const fname = "Suhas";
+const lname = "Palani";
+const lnum = 5;
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {fname}, {lname}!</h1>
+    {/* <h1>Hello {`${fname} ${lname}`}</h1> ES6 literals */}
+    <p>Your lucky number is {lnum}</p>
+  </div>,
+  document.getElementById("root")
+);
 
 // If you're running this locally in VS Code use the commands:
 // npm install
