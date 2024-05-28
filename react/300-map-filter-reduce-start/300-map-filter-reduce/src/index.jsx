eaf66d45b6de2685092714bee37de70db5ccf95c
SuhasPalani <sugipals007@gmail.com>
2024-05-28 16:36:38 -0500

    reduce

--- react/300-map-filter-reduce-start/300-map-filter-reduce/src/index.jsx
+++ react/300-map-filter-reduce-start/300-map-filter-reduce/src/index.jsx
@@ -18,13 +18,17 @@ var numbers = [3, 56, 2, 48, 5];
 // console.log(newNumbers);
 
 //Filter - Create a new array by keeping the items that return true.
-const newNumbers=numbers.filter(function(num) {
-  return num>10;
+// const newNumbers=numbers.filter(function(num) {
+//   return num>10;
+// });
+
+// console.log(newNumbers);
+//Reduce - Accumulate a value by doing something to each item in an array.
+const newNumbers = numbers.reduce(function (acc, cur) {
+  return acc + cur;
 });
 
 console.log(newNumbers);
-//Reduce - Accumulate a value by doing something to each item in an array.
-
 //Find - find the first item that matches from an array.
 
 //FindIndex - find the index of the first item that matches.
