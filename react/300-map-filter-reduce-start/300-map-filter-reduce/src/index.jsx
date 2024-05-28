4371637287d55795885256a204fafecd7bec459d
SuhasPalani <sugipals007@gmail.com>
2024-05-28 16:32:28 -0500

    filter

--- react/300-map-filter-reduce-start/300-map-filter-reduce/src/index.jsx
+++ react/300-map-filter-reduce-start/300-map-filter-reduce/src/index.jsx
@@ -2,14 +2,27 @@ var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
 
-const newNumbers = numbers.map(function (x) {
-  return x * 2;
+// const newNumbers = numbers.map(function (x) {
+//   return x * 2;
+// });
+
+// var newNumbers=[];
+// // function double(x){
+// //   newNumbers.push(x*2);
+// // }
+
+// numbers.forEach(function (x){
+//   newNumbers.push(x*2);
+// });
+
+// console.log(newNumbers);
+
+//Filter - Create a new array by keeping the items that return true.
+const newNumbers=numbers.filter(function(num) {
+  return num>10;
 });
 
 console.log(newNumbers);
-
-//Filter - Create a new array by keeping the items that return true.
-
 //Reduce - Accumulate a value by doing something to each item in an array.
 
 //Find - find the first item that matches from an array.
