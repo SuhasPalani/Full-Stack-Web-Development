d1759e9016073f39053565c21c685b308f03e73b
SuhasPalani <sugipals007@gmail.com>
2024-05-28 16:56:58 -0500

    emoji substring

--- react/300-map-filter-reduce-start/300-map-filter-reduce/src/index.jsx
+++ react/300-map-filter-reduce-start/300-map-filter-reduce/src/index.jsx
@@ -1,47 +1,54 @@
-var numbers = [3, 56, 2, 48, 5];
+// var numbers = [3, 56, 2, 48, 5];
 
-//Map -Create a new array by doing something with each item in an array.
+// //Map -Create a new array by doing something with each item in an array.
 
-// const newNumbers = numbers.map(function (x) {
-//   return x * 2;
-// });
+// // const newNumbers = numbers.map(function (x) {
+// //   return x * 2;
+// // });
 
-// var newNumbers=[];
-// // function double(x){
+// // var newNumbers=[];
+// // // function double(x){
+// // //   newNumbers.push(x*2);
+// // // }
+
+// // numbers.forEach(function (x){
 // //   newNumbers.push(x*2);
-// // }
+// // });
 
-// numbers.forEach(function (x){
-//   newNumbers.push(x*2);
-// });
+// // console.log(newNumbers);
 
-// console.log(newNumbers);
+// //Filter - Create a new array by keeping the items that return true.
+// // const newNumbers=numbers.filter(function(num) {
+// //   return num>10;
+// // });
 
-//Filter - Create a new array by keeping the items that return true.
-// const newNumbers=numbers.filter(function(num) {
-//   return num>10;
-// });
+// // console.log(newNumbers);
+// //Reduce - Accumulate a value by doing something to each item in an array.
+// // const newNumbers = numbers.reduce(function (acc, cur) {
+// //   return acc + cur;
+// // });
 
-// console.log(newNumbers);
-//Reduce - Accumulate a value by doing something to each item in an array.
-// const newNumbers = numbers.reduce(function (acc, cur) {
-//   return acc + cur;
-// });
+// // console.log(newNumbers);
+// //Find - find the first item that matches from an array.
 
-// console.log(newNumbers);
-//Find - find the first item that matches from an array.
+// const found = numbers.find((ele) => ele > 10);
 
-const found = numbers.find((ele) => ele > 10);
+// console.log(found);
+// //FindIndex - find the index of the first item that matches.
 
-console.log(found);
-//FindIndex - find the index of the first item that matches.
+// const founds = numbers.findIndex((ele) => ele > 10);
 
-const founds = numbers.findIndex((ele) => ele > 10);
+// console.log(founds);
 
-console.log(founds);
+// // If you're running this locally in VS Code use the commands:
+// // npm install
+// // to install the node modules and
+// // npm run dev
+// // to launch your react project in your browser
+import emojipedia from "./emojipedia";
+const newEp=emojipedia.map(function (em){
+  return em.meaning.substring(0,100)
+});
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+
+console.log(newEp)
