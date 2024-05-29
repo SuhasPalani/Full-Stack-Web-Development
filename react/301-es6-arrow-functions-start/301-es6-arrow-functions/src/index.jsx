2c9fda33cf28b0bcc37dc95fa0445f574aed1dfe
SuhasPalani <sugipals007@gmail.com>
2024-05-28 20:00:20 -0500

    solution practice

--- react/301-es6-arrow-functions-start/301-es6-arrow-functions/src/index.jsx
+++ react/301-es6-arrow-functions-start/301-es6-arrow-functions/src/index.jsx
@@ -4,32 +4,27 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// var numbers = [3, 56, 2, 48, 5];
+var numbers = [3, 56, 2, 48, 5];
 
-////Map -Create a new array by doing something with each item in an array.
-// const newNumbers = numbers.map(function (x) {
-//   return x * 2;
-// });
+//// Map - Create a new array by doing something with each item in an array.
+const doubledNumbers = numbers.map(x => x * 2);
+console.log("Doubled Numbers:", doubledNumbers);
 
-//////Filter - Create a new array by keeping the items that return true.
-// const newNumbers = numbers.filter(function(num) {
-//   return num < 10;
-// });
+//// Filter - Create a new array by keeping the items that return true.
+const filteredNumbers = numbers.filter(num => num < 10);
+console.log("Filtered Numbers:", filteredNumbers);
 
-//Reduce - Accumulate a value by doing something to each item in an array.
-// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
-//     return accumulator + currentNumber;
-// })
+//// Reduce - Accumulate a value by doing something to each item in an array.
+const sum = numbers.reduce((accumulator, currentNumber) => accumulator + currentNumber);
+console.log("Sum:", sum);
 
-////Find - find the first item that matches from an array.
-// const newNumber = numbers.find(function (num) {
-//   return num > 10;
-// })
+//// Find - Find the first item that matches from an array.
+const firstLargeNumber = numbers.find(num => num > 10);
+console.log("First Number Greater Than 10:", firstLargeNumber);
 
-////FindIndex - find the index of the first item that matches.
-// const newNumber = numbers.findIndex(function (num) {
-//   return num > 10;
-// })
+//// FindIndex - Find the index of the first item that matches.
+const firstLargeNumberIndex = numbers.findIndex(num => num > 10);
+console.log("Index of First Number Greater Than 10:", firstLargeNumberIndex);
 
 // If you're running this locally in VS Code use the commands:
 // npm install
