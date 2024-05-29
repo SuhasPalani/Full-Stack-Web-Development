910319d7e8edef133a497392f42547d71d15207b
SuhasPalani <sugipals007@gmail.com>
2024-05-29 18:26:20 -0500

    logical operators

--- react/303-conditional-rendering-start/303-conditional-rendering/src/components/App.jsx
+++ react/303-conditional-rendering-start/303-conditional-rendering/src/components/App.jsx
@@ -1,23 +1,20 @@
 import React from "react";
+import Login from "./Login";
 
 var isLoggedIn = false;
 
-function renderConditionally() {
-  if (isLoggedIn === true) {
-    return <h1>Hello</h1>;
-  } else {
-    return (
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    );
-  }
-}
+const currentTime = new Date(2019, 11, 1, 9).getHours();
+console.log(currentTime);
 
 function App() {
-  return <div className="container">{renderConditionally()}</div>;
+  return (
+    <div className="container">
+      {/*Ternary Operator*/}
+      {isLoggedIn ? <h1>Hello</h1> : <Login />}
+      {/*AND Operator*/}
+      {currentTime > 12 && <h1>Why are you still working?</h1>}
+    </div>
+  );
 }
 
 export default App;
