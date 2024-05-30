1446f244f6f314f28bd6af4167c19496686400fc
SuhasPalani <sugipals007@gmail.com>
2024-05-29 22:00:46 -0500

    solution

--- react/307-useState-hook-practice-start/307-useState-hook-practice/src/components/App.jsx
+++ react/307-useState-hook-practice-start/307-useState-hook-practice/src/components/App.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  function latestTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+
+  // Update time every second
+  setInterval(() => {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }, 1000);
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={latestTime}>Get Time</button>
     </div>
   );
 }
