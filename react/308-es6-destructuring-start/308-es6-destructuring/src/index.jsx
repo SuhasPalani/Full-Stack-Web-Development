3abdedfd3394d34ada186d28cd3d6ba44c1ea607
SuhasPalani <sugipals007@gmail.com>
2024-05-30 15:30:54 -0500

    practice

--- react/308-es6-destructuring-start/308-es6-destructuring/src/index.jsx
+++ react/308-es6-destructuring-start/308-es6-destructuring/src/index.jsx
@@ -1,26 +1,36 @@
 // CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
+import animals from "./data";
+// console.log(cars,animals)
 
-// ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
-//   document.getElementById("root")
-// );
+// const [cat, dog]=animals;
+
+
+
+
+
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
 
 // If you're running this locally in VS Code use the commands:
 // npm install
