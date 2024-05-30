817bf865aa81f733e0314593a5d6a8eada6cd1bc
SuhasPalani <sugipals007@gmail.com>
2024-05-29 21:48:25 -0500

    useState Hook solution

--- react/306-useState-hook-start/306-useState-hook/src/components/App.jsx
+++ react/306-useState-hook-start/306-useState-hook/src/components/App.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  // var count = 0;
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    // count++;
+    // console.log(count)
+    // ReactDOM.render(
+    //   <div className="container">
+    //     <h1>{count}</h1>
+    //     <button onClick={increase}>+</button>
+    //   </div>,
+    //   document.getElementById("root")
+    // );
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+    </div>
+  );
 }
 
 export default App;
